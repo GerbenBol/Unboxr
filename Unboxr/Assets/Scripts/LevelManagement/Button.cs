970a9ce828a592d47a17e6c4621a81d94f5bb324
Gerben Bol <gerben.bol249@gmail.com>
2024-03-20 09:56:50 +0100

    button improve + slide physics material

--- Unboxr/Assets/Scripts/LevelManagement/Button.cs
+++ Unboxr/Assets/Scripts/LevelManagement/Button.cs
@@ -7,20 +7,23 @@ public class Button : MonoBehaviour
     [SerializeField] new private MeshRenderer renderer;
     
     private Material wantedMaterial;
+    private string wantedName;
 
     private void Start()
     {
         wantedMaterial = renderer.material;
         Debug.Log(wantedMaterial.ToString());
+        wantedName = wantedMaterial.ToString().Split(' ')[0];
+        Debug.Log(wantedName);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Box"))
         {
-            Debug.Log(other.GetComponent<MeshRenderer>().material.ToString());
+            Debug.Log(other.GetComponent<MeshRenderer>().material.ToString().Split(' ')[0]);
 
-            if (other.GetComponent<MeshRenderer>().material == wantedMaterial)
+            if (other.GetComponent<MeshRenderer>().material.ToString().Split(' ')[0] == wantedName)
                 Debug.Log("Correct");
             else
             {
